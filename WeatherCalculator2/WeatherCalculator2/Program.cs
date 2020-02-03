43c350a7c21b14ee514e138d00746430259a7ed1
JustinKnust <jknust4@huskers.unl.edu>
2020-02-03 14:52:39 -0600

    Update Program.cs

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -12,30 +12,38 @@ namespace WeatherCalculator2
             int windSpeed;
             double windChill;
             string input = "";
+           double heatIndex;
+            int relHumidity;
 
             // Get the temperature from the user
             Console.WriteLine("Enter the temperature");
-
             input = Console.ReadLine();
 
-            temperature = int.Parse(input);
+            Console.WriteLine("Enter the relative humidity");
+            input = Console.ReadLine();
 
-            // Get the windspeed from the user
             Console.WriteLine("Enter the wind speed");
             string enteredtemp = Console.ReadLine();
 
-            windSpeed = int.Parse(input)
+            temperature = int.Parse(input);
+            if (temperature > 50)
+            {
+                heatIndex = .5 * (temperature + 61.0 + ((temperature - 6.08) * 1.2) + (relHumidity * 0.094));
+                Console.WriteLine("The heat index is" + heatIndex);
+            }
+            else (temperature < 50)
+            {
+                windSpeed = int.Parse(input);
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
 
-            ; double windchill = 35.74
-                + (0.6125 * temperature)
-                - 35.75 * Math.Pow(windSpeed, 0.16)
-             + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                Console.WriteLine("The wind chill is " + windChill);
 
-            Console.WriteLine("The wind chill is " + windchill);
+            }
+            // Get the windspeed from the user
+            
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+            
 
-            Console.WriteLine("The wind chill is " + windChill);
         }
     }
 }
